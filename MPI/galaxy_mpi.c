3d2c75dc676c3d4e9e816e27876648c6129ae7df
Clare <cad3@hw.ac.uk>
2021-10-26 17:55:22 +0300

    have reduce and broadcast in but still trouble getting them working properly in parrallel

--- MPI/galaxy_mpi.c
+++ MPI/galaxy_mpi.c
@@ -24,16 +24,14 @@
  * Definitions
  */
 #define MASTER 0         //One process will take care of initialization
-#define ARRAY_SIZE 10     //Size of arrays that will be added together.
-
 /*
  *  In MPI programs, the main function for the program is run on every
  *  process that gets initialized when you start up this code using mpirun.
  */
 float *real_rasc, *real_decl, *rand_rasc, *rand_decl;
-float *real_rasc_d, *real_decl_d, *rand_rasc_d, *rand_decl_d;
 float  pif;
 long int MemoryAllocatedCPU = 0L;
+double start,time,stop;
 
 int main (int argc, char *argv[]) 
 {
@@ -41,8 +39,9 @@ int main (int argc, char *argv[])
     int parseargs_readinput(int argc, char *argv[]);
     int get_index(float rasc_1, float decl_1, float rasc_2, float decl_2);
 
+    start = MPI_Wtime();
     pif = acosf(-1.0f);
-
+   
 	  long int histogram_DD[360] = {0L};
     long int histogram_DR[360] = {0L};
     long int histogram_RR[360] = {0L};
@@ -61,44 +60,30 @@ int main (int argc, char *argv[])
 	
   	int total_proc;	 // total nuber of processes	
   	int rank;        // rank of each process
-  	int n_per_proc;	// elements per process	
-  	int n = ARRAY_SIZE;   // number of array elements
   	int i,j;       // loop index
-  	int limit = 10;
-  	int count = 0;
+  	int limit = 100;
 
-  	MPI_Status status;   // not used in this arguably poor example
-  	                     // that is devoid of error checking.
+  	MPI_Status status;   
 
-  	// 1. Initialization of MPI environment
-  	MPI_Init (&argc, &argv);
-  	MPI_Comm_size (MPI_COMM_WORLD, &total_proc);
-  	// 2. Now you know the total number of processes running in parallel
-  	MPI_Comm_rank (MPI_COMM_WORLD,&rank);
-  	// 3. Now you know the rank of the current process
+  	MPI_Init (&argc, &argv);   // Initialization of MPI environment
+  	MPI_Comm_size (MPI_COMM_WORLD, &total_proc); //  total number of processes running in parallel
+  	MPI_Comm_rank (MPI_COMM_WORLD,&rank); //rank of the current process
   	
-  	// 4. We choose process rank 0 to be the root, or master,
-  	// which will be used to  initialize the full arrays.
   	if (rank == MASTER)  {
-  	    if ( parseargs_readinput(argc, argv) != 0 ) {
-  	    	printf("   Program stopped.\n");return(0);
-  	    }
-      	printf("Input data read, now calculating histograms\n");
-  	}
-  	n_per_proc = n/total_proc;
-	
-  	// 5. Initialize my smaller subsections of the larger array
-  	real_rasc_d = (float *) malloc(sizeof(float)*n_per_proc);
-  	real_decl_d = (float *) malloc(sizeof(float)*n_per_proc);
-  	rand_rasc_d = (float *) malloc(sizeof(float)*n_per_proc);
-  	rand_decl_d = (float *) malloc(sizeof(float)*n_per_proc);
-	
-  	// 6.
-  	//scattering array a from MASTER node out to the other nodes
-  	MPI_Scatter(real_rasc, n_per_proc, MPI_INT, real_rasc_d, n_per_proc, MPI_INT, MASTER, MPI_COMM_WORLD); 
-  	MPI_Scatter(real_decl, n_per_proc, MPI_INT, real_decl_d, n_per_proc, MPI_INT, MASTER, MPI_COMM_WORLD); 
-  	MPI_Scatter(rand_rasc, n_per_proc, MPI_INT, rand_rasc_d, n_per_proc, MPI_INT, MASTER, MPI_COMM_WORLD); 
-  	MPI_Scatter(rand_decl, n_per_proc, MPI_INT, rand_decl_d, n_per_proc, MPI_INT, MASTER, MPI_COMM_WORLD); 
+  	   if ( parseargs_readinput(argc, argv) != 0 ) {
+        printf("   Program stopped.\n");return(0);
+      }
+      printf("Input data read, now calculating histograms\n");
+    }
+
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    MPI_Bcast(&real_rasc, limit, MPI_INT, MASTER, MPI_COMM_WORLD);
+    MPI_Bcast(&real_decl, limit, MPI_INT, MASTER, MPI_COMM_WORLD);
+    MPI_Bcast(&rand_rasc, limit, MPI_INT, MASTER, MPI_COMM_WORLD);
+    MPI_Bcast(&rand_decl, limit, MPI_INT, MASTER, MPI_COMM_WORLD);
+
+    MPI_Barrier(MPI_COMM_WORLD);
 
     int range = limit/total_proc;
     int remainder = limit - (range*total_proc);
@@ -121,13 +106,7 @@ int main (int argc, char *argv[])
       end = (start-1) + range;
     }
 
-
-    printf ("rank %d \n", rank);
-    printf ("range %d \n", range);
-    printf ("total_proc %d \n", total_proc);
-    printf ("start %d \n", start);
-    printf ("end %d \n", end);
-	// for DR
+  // for DR
     for(i = start; i < end; ++i) 
     {
       for(j =start; j < end; ++j) 
@@ -136,55 +115,84 @@ int main (int argc, char *argv[])
       }
     }
 
-    // // for RR
-    // for(i = 0; i < n_per_proc; ++i) {
-    //   for(j = i+1; j < n_per_proc; ++j) {    
-    //     histogram_RR[get_index(rand_rasc[j], rand_decl[j], rand_rasc[j], rand_decl[j])] += 2;
-    //   }
-    // }
-    // histogram_RR[0] += limit;
+    // for RR
+    for(i = start; i < end; ++i) {
+      for(j = start; j < end; ++j) {   
+        ++histogram_RR[get_index(real_rasc[j], real_decl[j], real_rasc[j], real_decl[j])];
+      }
+    }
 
-    // // for DD
-    // for(i = 0; i < n_per_proc; ++i) {
-    //   for(j = i+1; j < n_per_proc; ++j) {  
-    //   	count++;
-    //     histogram_DD[get_index(real_rasc[j],real_decl[j],real_rasc[j],real_decl[j])] += 2;
-    //   }
-    // }
-    // histogram_DD[0] += limit;
-		
-    // may need to use reduction instead of gather
-	// 8. MASTER node gathering array c from the workers
-	// MPI_Gather(histogram_DD_total, n_per_proc, MPI_INT, histogram_DD, n_per_proc, MPI_INT, MASTER, MPI_COMM_WORLD);
-	 MPI_Gather(histogram_DR_total, n_per_proc, MPI_INT, histogram_DR, n_per_proc, MPI_INT, MASTER, MPI_COMM_WORLD);
-	// MPI_Gather(histogram_RR_total, n_per_proc, MPI_INT, histogram_RR, n_per_proc, MPI_INT, MASTER, MPI_COMM_WORLD);
-
-/////////////////////// all concurrent processes are finished once they all communicate
-/////////////////////// data back to the master via the gather function.
+    // for DD
+    for(i = start; i < end; ++i) {
+      for(j = start; j < end; ++j) {  
+        int index =  get_index(rand_rasc[j], rand_decl[j], rand_rasc[j], rand_decl[j]);
+        printf ("intially count %d \n", histogram_DD[index]);
+        ++histogram_DD[index];
+        printf ("now count (should go up) %d \n", histogram_DD[index]);
+      }
+    }
+    
+	 MPI_Reduce(histogram_DD,histogram_DD_total,limit,MPI_INT,MPI_SUM,MASTER,MPI_COMM_WORLD);
+	 MPI_Reduce(histogram_DR,histogram_DR_total,limit,MPI_INT,MPI_SUM,MASTER,MPI_COMM_WORLD);
+   MPI_Reduce(histogram_RR,histogram_RR_total,limit,MPI_INT,MPI_SUM,MASTER,MPI_COMM_WORLD);
 
 	// Master process gets to here only when it has been able to gather from all processes
-	if (rank == MASTER)  {			
-		// sanity check the result  (a test we would eventually leave out)
-		// int good = 1;
-		// for(i=0;i<n;i++) {
-		// 	//printf ("%d ", c[i]);
-		// 	if (c[i] != a[i] + b[i]) {
-		// 		printf("problem at index %lld\n", i);
-		// 		good = 0;
-		// 		break;
-		// 	}
-		// }
-		// if (good) {
-		// 	printf ("Values correct!\n");
-		// }
-		
+	if (rank == MASTER)  {	
+     // check point: the sum of all historgram entries should be 10 000 000 000
+    long int histsum = 0L;
+    int correct_value=1;
+    for ( int i = 0; i < 360; ++i ) histsum += histogram_DD[i];
+    printf("   Histogram DD : sum = %ld\n",histsum);
+    if ( histsum != 10000000000L ) correct_value = 0;
+
+    histsum = 0L;
+    for ( int i = 0; i < 360; ++i ) 
+      histsum += histogram_DR[i];
+    printf("   Histogram DR : sum = %ld\n",histsum);
+    if ( histsum != 10000000000L ) correct_value = 0;
+
+    histsum = 0L;
+    for ( int i = 0; i < 360; ++i ) histsum += histogram_RR[i];
+    printf("   Histogram RR : sum = %ld\n",histsum);
+    if ( histsum != 10000000000L ) correct_value = 0;
+
+    if ( correct_value != 1 ) 
+       {printf("   Histogram sums should be 10000000000. Ending program prematurely\n");return(0);}
+
+    printf("Omega values for the histograms:\n");
+    float omega[360];
+    for ( int i = 0; i < 360; ++i ) 
+    {
+        if ( histogram_RR[i] != 0L )
+        {
+           omega[i] = (histogram_DD[i] - 2L*histogram_DR[i] + histogram_RR[i])/((float)(histogram_RR[i]));
+           if ( i < 10 ) 
+            printf("   angle %.2f deg. -> %.2f deg. : %.3f\n", i*0.25, (i+1)*0.25, omega[i]);
+        }
+    }
+
+    FILE *out_file = fopen(argv[3],"w");
+    if ( out_file == NULL ) printf("   ERROR: Cannot open output file %s\n",argv[3]);
+    else
+       {
+       for ( int i = 0; i < 360; ++i ) 
+       {
+           if ( histogram_RR[i] != 0L )
+              fprintf(out_file,"%.2f  : %.3f\n", i*0.25, omega[i] ); 
+       }
+          
+       fclose(out_file);
+       printf("   Omega values written to file %s\n",argv[3]);
+       }
+       
+    stop = MPI_Wtime();
+    time = stop-start;	
 	}
 
 	// clean up memory
 	if (rank == MASTER)  {
 		free(real_rasc);  free(real_decl); free(rand_rasc); free(rand_decl);
 	}
-	free(real_rasc_d);  free(real_decl_d); free(rand_rasc_d); free(rand_decl_d);
 	
 	// 9. Terminate MPI Environment and Processes
 	MPI_Finalize();  
@@ -193,93 +201,83 @@ int main (int argc, char *argv[])
 }
 
 	int get_index(float rasc_1, float decl_1, float rasc_2, float decl_2)
-    {
-        float calcAngle = 0;
+  {
+      float calcAngle = 0;
     	calcAngle = ((sin(decl_1)*sin(decl_2))+(cos(decl_1)*cos(decl_2))*cos(rasc_1-rasc_2));
    		if(calcAngle > 1.0)
           calcAngle = 1.0;
-        else if(calcAngle < -1.0)
-          calcAngle = -1.0;
-        return (int) 4*acos(calcAngle)*180.0/pif;
-    }
+      else if(calcAngle < -1.0)
+        calcAngle = -1.0;
+      return (int) 4*acos(calcAngle)*180.0/pif;
+  }
 
 
 int parseargs_readinput(int argc, char *argv[])
-    {
+{
     FILE *real_data_file, *rand_data_file, *out_file;
     float arcmin2rad = 1.0f/60.0f/180.0f*pif;
     int Number_of_Galaxies;
   
-    if ( argc != 4 ) 
-       {
+    if ( argc != 4 ) {
        printf("   Usage: galaxy real_data random_data output_file\n   All MPI processes will be killed\n");
        return(1);
-       }
-    if ( argc == 4 )
-       {
+    }
+    if ( argc == 4 ){
        printf("   Running galaxy_openmp %s %s %s\n",argv[1], argv[2], argv[3]);
 
        real_data_file = fopen(argv[1],"r");
-       if ( real_data_file == NULL ) 
-          {
+       if ( real_data_file == NULL )  {
           printf("   Usage: galaxy  real_data  random_data  output_file\n");
           printf("   ERROR: Cannot open real data file %s\n",argv[1]);
           return(1);
-          }
+      }
        else
     	{
-          fscanf(real_data_file,"%d",&Number_of_Galaxies);
-          for ( int i = 0; i < 100000; ++i ) 
-              {
-              float rasc, decl;
-        if ( fscanf(real_data_file,"%f %f", &rasc, &decl ) != 2 )
-           {
-                 printf("   ERROR: Cannot read line %d in real data file %s\n",i+1,argv[1]);
-                 fclose(real_data_file);
-           return(1);
-           }
-        real_rasc[i] = rasc*arcmin2rad;
-        real_decl[i] = decl*arcmin2rad;
-
+        fscanf(real_data_file,"%d",&Number_of_Galaxies);
+        for ( int i = 0; i < 100000; ++i ) {
+          float rasc, decl;
+          if ( fscanf(real_data_file,"%f %f", &rasc, &decl ) != 2 ){
+               printf("   ERROR: Cannot read line %d in real data file %s\n",i+1,argv[1]);
+               fclose(real_data_file);
+             return(1);
+          }
+          real_rasc[i] = rasc*arcmin2rad;
+          real_decl[i] = decl*arcmin2rad;
         }
-           fclose(real_data_file);
-     printf("   Successfully read 100000 lines from %s\n",argv[1]);
+        fclose(real_data_file);
+        printf("   Successfully read 100000 lines from %s\n",argv[1]);
      }
 
        rand_data_file = fopen(argv[2],"r");
-       if ( rand_data_file == NULL ) 
-          {
+       if ( rand_data_file == NULL ) {
           printf("   Usage: galaxy  real_data  random_data  output_file\n");
           printf("   ERROR: Cannot open random data file %s\n",argv[2]);
           return(1);
-          }
-       else 
-    {
-          fscanf(rand_data_file,"%d",&Number_of_Galaxies);
-          for ( int i = 0; i < 100000; ++i ) 
-              {
-              float rasc, decl;
-        if ( fscanf(rand_data_file,"%f %f", &rasc, &decl ) != 2 )
-           {
-                 printf("   ERROR: Cannot read line %d in real data file %s\n",i+1,argv[2]);
-                 fclose(rand_data_file);
-           return(1);
-           }
-        rand_rasc[i] = rasc*arcmin2rad;
-        rand_decl[i] = decl*arcmin2rad;
         }
-          fclose(rand_data_file);
-    printf("   Successfully read 100000 lines from %s\n",argv[2]);
+       else {
+          fscanf(rand_data_file,"%d",&Number_of_Galaxies);
+          for ( int i = 0; i < 100000; ++i ) {
+            float rasc, decl;
+            if ( fscanf(rand_data_file,"%f %f", &rasc, &decl ) != 2 )
+               {
+                     printf("   ERROR: Cannot read line %d in real data file %s\n",i+1,argv[2]);
+                     fclose(rand_data_file);
+               return(1);
+               }
+            rand_rasc[i] = rasc*arcmin2rad;
+            rand_decl[i] = decl*arcmin2rad;
+        }
+        fclose(rand_data_file);
+        printf("   Successfully read 100000 lines from %s\n",argv[2]);
+      }
+      out_file = fopen(argv[3],"w");
+      if ( out_file == NULL ) {
+        printf("   Usage: galaxy  real_data  random_data  output_file\n");
+        printf("   ERROR: Cannot open output file %s\n",argv[3]);
+        return(1);
+        }
+      else fclose(out_file);
     }
-       out_file = fopen(argv[3],"w");
-       if ( out_file == NULL ) 
-          {
-          printf("   Usage: galaxy  real_data  random_data  output_file\n");
-          printf("   ERROR: Cannot open output file %s\n",argv[3]);
-          return(1);
-          }
-       else fclose(out_file);
-       }
 	
-	return 0;
+	  return 0;
 }
