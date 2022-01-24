603c501765b64397e3c03a322335934d6639ba4a
claredavies <clare.davies@abo.fi>
2022-01-24 14:00:18 +0000

    able to write to output file to sum of histograms incorrect

--- CUDA/vecAdd.cu
+++ CUDA/vecAdd.cu
@@ -10,8 +10,6 @@ int binsperdegree = 4;
 float *h_real_rasc, *h_real_decl;
 float *h_rand_rasc, *h_rand_decl;
 
-long int CPUMemory = 0L;
-
 // CUDA kernel. Each thread takes care of one element of c
 __global__ void vecAdd(float *real_rasc, float *real_decl, float *rand_rasc, float *rand_decl, int *histogram_DD
     ,int *histogram_DR, int *histogram_RR, int n)
@@ -20,11 +18,13 @@ __global__ void vecAdd(float *real_rasc, float *real_decl, float *rand_rasc, flo
     int idx = blockIdx.x*blockDim.x+threadIdx.x;
 
     // Make sure we do not go out of bounds
-    printf("id:   %d    N:  %d \n",idx,n);
     int j;
     if (idx < n) {
+        printf("id:   %d    N:  %d \n",idx,n);
          for(j =0; j < n; ++j) {
             atomicAdd(&(histogram_DR[idx]),(int)1);
+            atomicAdd(&(histogram_DD[idx]),(int)1);
+            atomicAdd(&(histogram_RR[idx]),(int)1);
          }
      }
 }
@@ -32,27 +32,26 @@ __global__ void vecAdd(float *real_rasc, float *real_decl, float *rand_rasc, flo
 int main( int argc, char* argv[] )
 {
     int readdata(char *argv1, char *argv2);
+    int getDevice(void);
+
     // Size of vectors
     int n = 10;
-    //int readdata(char *argv1, char *argv2);
 
     //Host output vector
     int *h_histogram_DR, *h_histogram_DD, *h_histogram_RR;
 
-    if ( readdata(argv[1], argv[2]) != 0 ) return(-1);
+    FILE *outfil;
+    if ( argc != 4 ) {printf("Usage: a.out real_data random_data output_data\n");return(-1);}
 
     // Device input vectors
-    float *d_real_rasc;
-    float *d_real_decl;
-    float *d_rand_rasc;
-    float *d_rand_decl;
+    float *d_real_rasc; float *d_real_decl; float *d_rand_rasc; float *d_rand_decl;
 
     //Device output vector
     int *d_histogram_DR, *d_histogram_DD, *d_histogram_RR;
  
     // Size, in bytes, of each vector
     size_t bytes = n*sizeof(double);
-    size_t size_long_int = n*sizeof(long int);
+    //size_t size_long_int = n*sizeof(long int);
     size_t size_int = n*sizeof(int);
  
     // Allocate memory for each vector on host
@@ -73,8 +72,13 @@ int main( int argc, char* argv[] )
     cudaMalloc(&d_histogram_DD, size_int );
     cudaMalloc(&d_histogram_DR, size_int );
     cudaMalloc(&d_histogram_RR, size_int );
- 
+
+    printf("here 1");
+    if ( getDevice() != 0 ) return(-1);
+
     if ( readdata(argv[1], argv[2]) != 0 ) return(-1);
+
+    printf("here 2");
 
     // Copy host vectors to device
     cudaMemcpy(d_real_rasc, h_real_rasc, bytes, cudaMemcpyHostToDevice);
@@ -99,37 +103,71 @@ int main( int argc, char* argv[] )
     cudaMemcpy( h_histogram_DR, d_histogram_DR, size_int, cudaMemcpyDeviceToHost );
     cudaMemcpy( h_histogram_RR, d_histogram_RR, size_int, cudaMemcpyDeviceToHost );
 
-    // Sum up vector c and print result divided by n, this should equal 1 within error
-    double sum_DD = 0;
-    double sum_DR = 0;
-    double sum_RR = 0;
-    int i;
-    for(i=0; i<n; i++) {
-         sum_DD += h_histogram_DD[i];
-         sum_DR += h_histogram_DR[i];
-         sum_RR += h_histogram_RR[i];
-    }
-    printf("final result DD: %f  final result DR: %f  final result RR: %f\n", sum_DD,sum_DR,sum_RR);
- 
-    // Release device memory
-    cudaFree(d_real_rasc);
-    cudaFree(d_real_decl);
-    cudaFree(d_rand_rasc);
-    cudaFree(d_rand_decl);
-    cudaFree(d_histogram_DD);
-    cudaFree(d_histogram_DR);
-    cudaFree(d_histogram_RR);
+    int histogramDRsum, histogramDDsum, histogramRRsum;
+    histogramDRsum = 0L;
+    for ( int i = 0; i < binsperdegree*totaldegrees;++i )
+        histogramDRsum += h_histogram_DR[i];
+    printf("   DR histogram sum = %ld\n",histogramDRsum);
+    if ( histogramDRsum != 10000000000L ) {printf("   Incorrect histogram sum, exiting..\n");return(0);}
 
- 
-    // Release host memory
-    free(h_real_rasc);
-    free(h_real_decl);
-    free(h_rand_rasc);
-    free(h_rand_decl);
-    free(h_histogram_DD);
-    free(h_histogram_DR);
-    free(h_histogram_RR);
- 
+    histogramDDsum = 0L;
+    for ( int i = 0; i < binsperdegree*totaldegrees;++i )
+        histogramDDsum += h_histogram_DD[i];
+    printf("   DD histogram sum = %ld\n",histogramDDsum);
+    if ( histogramDDsum != 10000000000L ) {printf("   Incorrect histogram sum, exiting..\n");return(0);}
+
+    histogramRRsum = 0L;
+    for ( int i = 0; i < binsperdegree*totaldegrees;++i )
+        histogramRRsum += h_histogram_RR[i];
+    printf("   RR histogram sum = %ld\n",histogramRRsum);
+
+    if ( histogramRRsum != 10000000000L ) {printf("   Incorrect histogram sum, exiting..\n");return(0);}
+
+
+   printf("   Omega values:");
+
+   outfil = fopen(argv[3],"w");
+   if ( outfil == NULL ) {printf("Cannot open output file %s\n",argv[3]);return(-1);}
+   fprintf(outfil,"bin start\tomega\t        hist_DD\t        hist_DR\t        hist_RR\n");
+   for ( int i = 0; i < binsperdegree*totaldegrees; ++i )
+       {
+       if (h_histogram_RR[i] > 0 )
+          {
+          double omega =  (h_histogram_DD[i]-2*h_histogram_DR[i]+h_histogram_RR[i])/((double)(h_histogram_RR[i]));
+
+          fprintf(outfil,"%6.3f\t%15lf\t%15ld\t%15ld\t%15ld\n",((float)i)/binsperdegree, omega,
+             h_histogram_DD[i], h_histogram_DR[i], h_histogram_RR[i]);
+          if ( i < 5 ) printf("   %6.4lf",omega);
+          }
+       else
+          if ( i < 5 ) printf("         ");
+       }
+
+       printf("\n");
+
+       fclose(outfil);
+
+       printf("   Results written to file %s\n",argv[3]);
+
+     // Release device memory
+        cudaFree(d_real_rasc);
+        cudaFree(d_real_decl);
+        cudaFree(d_rand_rasc);
+        cudaFree(d_rand_decl);
+        cudaFree(d_histogram_DD);
+        cudaFree(d_histogram_DR);
+        cudaFree(d_histogram_RR);
+
+
+        // Release host memory
+        free(h_real_rasc);
+        free(h_real_decl);
+        free(h_rand_rasc);
+        free(h_rand_decl);
+        free(h_histogram_DD);
+        free(h_histogram_DR);
+        free(h_histogram_RR);
+
     return 0;
 }
 
@@ -163,7 +201,6 @@ int readdata(char *argv1, char *argv2)
 
   h_real_rasc = (float *)calloc(NoofReal,sizeof(float));
   h_real_decl = (float *)calloc(NoofReal,sizeof(float));
-  CPUMemory += 2L*NoofReal*sizeof(float);
 
   fgets(inbuf,80,infil);
   sscanf(inbuf,"%d",&linecount);
@@ -205,7 +242,6 @@ int readdata(char *argv1, char *argv2)
 
   h_rand_rasc = (float *)calloc(NoofRand,sizeof(float));
   h_rand_decl = (float *)calloc(NoofRand,sizeof(float));
-  CPUMemory += 2L*NoofRand*sizeof(float);
 
   fgets(inbuf,80,infil);
   sscanf(inbuf,"%d",&linecount);
@@ -232,6 +268,22 @@ int readdata(char *argv1, char *argv2)
       printf("   Cannot read %s correctly\n",argv2);
       return(-1);
       }
+
+  return(0);
+}
+
+int getDevice(void)
+{
+
+  int deviceCount;
+  cudaGetDeviceCount(&deviceCount);
+  printf("   Found %d CUDA devices\n",deviceCount);
+  if ( deviceCount < 0 || deviceCount > 128 ) return(-1);
+  int device;
+  cudaSetDevice(0);
+  cudaGetDevice(&device);
+  if ( device != 0 ) printf("   Unable to set device 0, using %d instead",device);
+  else printf("   Using CUDA device %d\n\n", device);
 
   return(0);
 }
