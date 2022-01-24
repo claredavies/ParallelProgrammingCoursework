d65aecf9163c1f49b4c5738d2161076cd7242427
claredavies <clare.davies@abo.fi>
2022-01-24 16:08:11 +0000

    able to get right sum for up to 100000

--- CUDA/vecAdd.cu
+++ CUDA/vecAdd.cu
@@ -9,6 +9,7 @@ int totaldegrees = 360;
 int binsperdegree = 4;
 float *h_real_rasc, *h_real_decl;
 float *h_rand_rasc, *h_rand_decl;
+__device__ int get_index(float rasc_1, float decl_1, float rasc_2, float decl_2);
 
 // CUDA kernel. Each thread takes care of one element of c
 __global__ void vecAdd(float *real_rasc, float *real_decl, float *rand_rasc, float *rand_decl, int *histogram_DD
@@ -18,15 +19,28 @@ __global__ void vecAdd(float *real_rasc, float *real_decl, float *rand_rasc, flo
     int idx = blockIdx.x*blockDim.x+threadIdx.x;
 
     // Make sure we do not go out of bounds
-    int j;
     if (idx < n) {
-        printf("id:   %d    N:  %d \n",idx,n);
-         for(j =0; j < n; ++j) {
-            atomicAdd(&(histogram_DR[idx]),(int)1);
-            atomicAdd(&(histogram_DD[idx]),(int)1);
-            atomicAdd(&(histogram_RR[idx]),(int)1);
-         }
+        printf("ID:   %d \n",idx);
+        for(int j =0; j < n; ++j) {
+                atomicAdd(&(histogram_DR[0]),(int)1);
+        }
+        //    printf("j:   %d \n",j);
+        //    atomicAdd(&(histogram_DR[get_index(real_rasc[idx], real_decl[idx], rand_rasc[j], rand_decl[j])]),(int)1);
+        //
      }
+}
+
+__device__ int get_index(float rasc_1, float decl_1, float rasc_2, float decl_2)
+{
+      float  pif;
+      pif = acosf(-1.0f);
+      float calcAngle = 0;
+    	calcAngle = ((sin(decl_1)*sin(decl_2))+(cos(decl_1)*cos(decl_2))*cos(rasc_1-rasc_2));
+   		if(calcAngle > 1.0)
+          calcAngle = 1.0;
+      else if(calcAngle < -1.0)
+        calcAngle = -1.0;
+      return (int) 4*acos(calcAngle)*180.0/pif;
 }
  
 int main( int argc, char* argv[] )
@@ -34,8 +48,11 @@ int main( int argc, char* argv[] )
     int readdata(char *argv1, char *argv2);
     int getDevice(void);
 
+    printf("here 1");
+    if ( getDevice() != 0 ) return(-1);
+
     // Size of vectors
-    int n = 10;
+    int n = 10000;
 
     //Host output vector
     int *h_histogram_DR, *h_histogram_DD, *h_histogram_RR;
@@ -60,9 +77,9 @@ int main( int argc, char* argv[] )
     h_rand_rasc = (float*)malloc(bytes);
     h_rand_decl = (float*)malloc(bytes);
 
-    h_histogram_DD = (int*)malloc(size_int);
-    h_histogram_DR = (int*)malloc(size_int);
-    h_histogram_RR = (int*)malloc(size_int);
+    h_histogram_DD = (int*)calloc((totaldegrees*binsperdegree+1L),size_int);
+    h_histogram_DR = (int*)calloc((totaldegrees*binsperdegree+1L),size_int);
+    h_histogram_RR = (int*)calloc((totaldegrees*binsperdegree+1L),size_int);
 
     // Allocate memory for each vector on GPU
     cudaMalloc(&d_real_rasc, bytes);
@@ -73,12 +90,9 @@ int main( int argc, char* argv[] )
     cudaMalloc(&d_histogram_DR, size_int );
     cudaMalloc(&d_histogram_RR, size_int );
 
-    printf("here 1");
-    if ( getDevice() != 0 ) return(-1);
+    printf("here 2");
 
     if ( readdata(argv[1], argv[2]) != 0 ) return(-1);
-
-    printf("here 2");
 
     // Copy host vectors to device
     cudaMemcpy(d_real_rasc, h_real_rasc, bytes, cudaMemcpyHostToDevice);
@@ -104,11 +118,16 @@ int main( int argc, char* argv[] )
     cudaMemcpy( h_histogram_RR, d_histogram_RR, size_int, cudaMemcpyDeviceToHost );
 
     int histogramDRsum, histogramDDsum, histogramRRsum;
-    histogramDRsum = 0L;
-    for ( int i = 0; i < binsperdegree*totaldegrees;++i )
-        histogramDRsum += h_histogram_DR[i];
+
+    histogramDRsum = 0;
+    for ( int i = 0; i < binsperdegree*totaldegrees;++i ) {
+         histogramDRsum += h_histogram_DR[i];
+    }
     printf("   DR histogram sum = %ld\n",histogramDRsum);
-    if ( histogramDRsum != 10000000000L ) {printf("   Incorrect histogram sum, exiting..\n");return(0);}
+    if ( histogramDRsum != 10000000000L )
+     {   printf("   Incorrect histogram sum, exiting..\n");
+         return(0);
+     }
 
     histogramDDsum = 0L;
     for ( int i = 0; i < binsperdegree*totaldegrees;++i )
@@ -128,6 +147,7 @@ int main( int argc, char* argv[] )
 
    outfil = fopen(argv[3],"w");
    if ( outfil == NULL ) {printf("Cannot open output file %s\n",argv[3]);return(-1);}
+
    fprintf(outfil,"bin start\tomega\t        hist_DD\t        hist_DR\t        hist_RR\n");
    for ( int i = 0; i < binsperdegree*totaldegrees; ++i )
        {
@@ -157,7 +177,6 @@ int main( int argc, char* argv[] )
         cudaFree(d_histogram_DD);
         cudaFree(d_histogram_DR);
         cudaFree(d_histogram_RR);
-
 
         // Release host memory
         free(h_real_rasc);
